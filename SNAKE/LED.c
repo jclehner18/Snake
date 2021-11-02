2e71b58677c3bd67085a9312db821e48d0cfe221
jack <jclehner18@my.trine.edu>
2021-11-02 15:56:03 -0400

    added some comments to led

--- SNAKE/LED.c
+++ SNAKE/LED.c
@@ -1,2 +1,21 @@
 //LED code will appear here
+//adjust the brightness of the led when a fruit is hit.
+//need pwm
+//need to adjust duty cycle
+//implement interrupt in main.c. when fruit is hit interrupt starts and communicates to led
 
+
+//use tim21
+//use gpio PC0
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stm32l053xx.h"
+#include <stdint.h>
+void led();
+
+
+void led() //main file for this file
+{
+	
+}
