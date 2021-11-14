8593621c6c5a4733e3cd9bb70f48590dfb52e092
jclehner18 <jclehner18@my.trine.edu>
2021-11-14 18:25:15 -0500

    More final comments

--- SNAKE/knob.h
+++ SNAKE/knob.h
@@ -3,5 +3,5 @@
 #include "stdint.h"
 #include "fsm_btn.h"
 
-void gpio_init(void);
-void knob(void);
+void gpio_init(void); //GPIO configuration for Pins 8 and 9. gpio_init();
+void knob(void); //accepts user input to change snake direction. knob();
