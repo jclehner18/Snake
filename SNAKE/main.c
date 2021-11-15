2e79ae5ad156a9d89a1228c28b7859f3ca13de5d
jclehner18 <jclehner18@my.trine.edu>
2021-11-15 14:11:36 -0500

    Added Wiring description to main.c

--- SNAKE/main.c
+++ SNAKE/main.c
@@ -1,7 +1,5 @@
 // main file for running the schedule
 
-
-
 /* Copyright 2021 John Lehner & Josiah Mortorff
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,8 +15,6 @@
    limitations under the License.
 */
 
-
-
 //HARDWARE REQUIREMENTS
 //stm32l053R8 Microcontroller
 //breadboard
@@ -26,7 +22,31 @@
 //PEC12R - 12mm Incremental Encoder - x2 10k Ohm Resistors - x2 0.01uF Capacitors
 //LED - 330 Ohm Resistor
 
-//WIRING
+//DISPLAY WIRING - display to stm32
+//PIN 15 to 1uF capacitor to GND
+//PIN 16 (VB1-) to 1uF capacitor to PIN 19 (VB1+)
+//PIN 17 (VB0-) to 1uF capacitor to PIN 18 (VB0+)
+//PIN 20 (VSS) to GND
+//PIN 21 (VSS) to GND
+//PIN 22 (VDD2/3) to 3.3v
+//PIN 23 (VDD1) to 3.3v
+//PIN 24 (SDA) to PA7
+//PIN 25 (SCK) to PA5
+//PIN 26 (CD) to PA1
+//PIN 27 (RST) to PA0
+//PIN 28 (CS0) to PA4
+
+//KNOB WIRING - knob to stm32
+//TERMINAL A to 10k Ohm resistor to 0.01uF capacitor to GND
+//TERMINAL A to PB8
+//TERMINAL B to 10k Ohm resistor to 0.01uF capacitor to GND
+//TERMINAL B to PB9
+//TERMINAL C to GND
+
+//LED WIRING - LED to stm32
+//Anode to 330 Ohm Resistor to PB13
+//Cathode to GND
+
 
 #include "main.h"
 queue_t Direction;// queue for use from knob to game logic
